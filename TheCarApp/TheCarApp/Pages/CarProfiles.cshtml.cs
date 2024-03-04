6d3bf215a0a3902758fdf8b37bffe447cb6a6919
ATANAS DIMITROV <i530410@fontysict.nl>
2024-03-04 14:56:26 +0100

    Razor Pages

---
+++ TheCarApp/TheCarApp/Pages/CarProfiles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheCarApp.Pages
+{
+    public class CarProfilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
