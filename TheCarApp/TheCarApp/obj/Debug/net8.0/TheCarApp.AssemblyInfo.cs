be6c7655cdb8694c3f38635e6c210b51a57e48f3
Atanas Dimitrov <i530410@fontysict.nl>
2024-03-17 17:03:02 +0200

    Some Changes

--- TheCarApp/TheCarApp/obj/Debug/net8.0/TheCarApp.AssemblyInfo.cs
+++ TheCarApp/TheCarApp/obj/Debug/net8.0/TheCarApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheCarApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7be06ad15683838e174559d080d58541abcc78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheCarApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheCarApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
