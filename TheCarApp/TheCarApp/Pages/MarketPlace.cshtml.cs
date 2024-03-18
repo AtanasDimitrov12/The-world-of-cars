d5215a6343eb295475c1b3595da379eafbf0f215
ATANAS DIMITROV <i530410@fontysict.nl>
2024-03-18 13:56:00 +0100

    Some footer changes

--- TheCarApp/TheCarApp/Pages/MarketPlace.cshtml.cs
+++ TheCarApp/TheCarApp/Pages/MarketPlace.cshtml.cs
@@ -8,11 +8,10 @@ namespace TheCarApp.Pages
 {
     public class MarketPlaceModel : PageModel
     {
-        public List<Car> Cars = new List<Car>(); // This should ideally be a list of SpecificCar
+        public List<Car> Cars = new List<Car>(); 
 
         public void OnGet()
         {
-            // Add some cars to the list
             Cars.Add(new SpecificCar(CarBrands.Audi, "A6", 2000, 140000, "Diesel", 2000, 250, "Automatic", "Blue", 15000));
             Cars.Add(new SpecificCar(CarBrands.BMW, "230", 2010, 140000, "Petrol", 2500, 300, "Automatic", "Blue", 20000));
             Cars.Add(new SpecificCar(CarBrands.Audi, "A6", 2000, 140000, "Diesel", 2000, 250, "Automatic", "Blue", 15000));
