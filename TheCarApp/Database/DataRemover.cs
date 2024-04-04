547c9463bbba8372e207677a69bc216a2c099c9b
ATANAS DIMITROV <i530410@fontysict.nl>
2024-04-04 11:08:45 +0200

    Adding methods to add Car in DataWriter

--- TheCarApp/Database/DataRemover.cs
+++ TheCarApp/Database/DataRemover.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseAccess
 {
-    internal class DataRemover
+    public class DataRemover
     {
+        private SqlConnection connectionString;
+
+        public DataRemover()
+        { 
+            connectionString = new SqlConnection("Server=mssqlstud.fhict.local;Database=dbi530410_carapp;User Id=dbi530410_carapp;Password=Fontyspass;TrustServerCertificate=True;");
+        }
     }
 }
