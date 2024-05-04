0fd3b4a514701258dcf0719ea4bbbfd905d1a937
ATANAS DIMITROV <i530410@fontysict.nl>
2024-05-04 17:20:39 +0300

    add dgv

--- TheCarApp/WinDesktopApp/obj/Debug/net8.0-windows/WinDesktopApp.AssemblyInfo.cs
+++ TheCarApp/WinDesktopApp/obj/Debug/net8.0-windows/WinDesktopApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759812ace21467a707d468a12833441803331ba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd18ecd3a011280b6ed58688d356e1f9669bcabf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
