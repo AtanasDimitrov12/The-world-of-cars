364b16807d00c7ff5b4d75b7152bb00e28b70d7e
ATANAS DIMITROV <i530410@fontysict.nl>
2024-06-02 17:05:14 +0200

    Fixing bugs in updating the information

--- TheCarApp/ManagerLayer/obj/Debug/net8.0/ManagerLayer.AssemblyInfo.cs
+++ TheCarApp/ManagerLayer/obj/Debug/net8.0/ManagerLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d907a4081823fe8a66cd098a673fce7d4a786db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
