4baacefa615a53cb475df17c085e0d6bff050540
Atanas Dimitrov <530410@student.fontys.nl>
2024-10-25 15:39:44 +0300

    commit some auto changes

--- TheCarApp/ManagerLayer/obj/Debug/net8.0/ManagerLayer.AssemblyInfo.cs
+++ TheCarApp/ManagerLayer/obj/Debug/net8.0/ManagerLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc1666d10250c68ce4dafb3cf992d5553377aa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9fc7fe7d037c074e687735c06c6dc549096126")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagerLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
