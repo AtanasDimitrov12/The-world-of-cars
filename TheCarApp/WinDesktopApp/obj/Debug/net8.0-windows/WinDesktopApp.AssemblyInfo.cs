898afe076b9fe7ecf8014d59d4f16bce91ce0c7a
Atanas Dimitrov <530410@student.fontys.nl>
2024-10-25 15:47:58 +0300

    auto changes try to fix this issue

--- TheCarApp/WinDesktopApp/obj/Debug/net8.0-windows/WinDesktopApp.AssemblyInfo.cs
+++ TheCarApp/WinDesktopApp/obj/Debug/net8.0-windows/WinDesktopApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9fc7fe7d037c074e687735c06c6dc549096126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4baacefa615a53cb475df17c085e0d6bff050540")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinDesktopApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
